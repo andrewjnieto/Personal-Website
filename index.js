1e6dea0751166440c1b2f6d798ce533e9b288183
Andrew Nieto <andrewjnieto@gmail.com>
2020-06-01 13:52:27 -0400

    Added Navbar and Changed Header

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
-	$('.top-content').backstretch("assets/backgrounds/background.jpg");
+	$('.top-content').backstretch("assets/backgrounds/background6.jpg");
+	var navbar = document.getElementById("resume-navbar");
+	var sticky = navbar.offsetTop;
+	window.onscroll = () => {
+	  if (window.pageYOffset >= sticky) {
+	    navbar.style.position = "fixed";
+	    navbar.style.top = "0";
+	  } else {
+	   	navbar.style.position = "absolute";
+	    navbar.style.top = "100%";
+	}
+	document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+}
 });
