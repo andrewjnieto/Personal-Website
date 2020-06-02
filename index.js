af184a2ad6c14796e5ba199b75bf2ec2b7da42d2
Andrew Nieto <andrewjnieto@gmail.com>
2020-06-02 16:02:24 -0400

    Updated the navbar and the technology section
    Added the ability for the navbar to change depending on how the user clicked(css/js) on the anchors or scrolled through the page(js/jquery). Furthermore, added pictures to the languages subsection of the 'languages and technology' section of the resume

--- index.js
+++ index.js
@@ -2,22 +2,56 @@ $(document).ready(function () {
 	$('.top-content').backstretch("assets/backgrounds/background6.jpg");
 	var navbar = document.getElementById("resume-navbar");
 	var sticky = navbar.offsetTop;
-	window.onscroll = () => {
-	  if (window.pageYOffset >= sticky) {
-	    navbar.style.position = "fixed";
-	    navbar.style.top = "0";
-	  } else {
-	   	navbar.style.position = "absolute";
-	    navbar.style.top = "100%";
-	}
-	document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-}
+	//If the view goes past a certain point, 
+	window.onscroll = () => {
+		if (window.pageYOffset >= sticky) {
+			navbar.style.position = "fixed";
+			navbar.style.top = "0";
+		} else {
+			navbar.style.position = "absolute";
+			navbar.style.top = "100%";
+		}
+	};
+
+	//If the user clicks on one of the links on the navbar, will have a smooth scroll animation.
+	document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+		anchor.addEventListener('click', function (e) {
+			e.preventDefault();
+			document.querySelector(this.getAttribute('href')).scrollIntoView({
+				behavior: 'smooth'
+			});
+		});
+	});
+
+	//If the user clicks on one of the links on the navbar,
+	// the active class will be added to the list item.
+	$('.navbar-nav li').on('click', function(event) {
+		$(this).parents().find('li').removeClass('active');
+		$(this).addClass('active');
+	});
+
+	var topMenuHeight = $("#resume-navbar").outerHeight()+15,
+	scrollItems = $("#resume-navbar").find("a").map(function(){
+		var item = $($(this).attr("href"));
+		if (item.length) { 
+			return item; 
+		}
+	});
+	$(window).scroll(function(){
+	   // Get container scroll position
+	   var fromTop = $(this).scrollTop()+topMenuHeight;
+
+	   // Get id of current scroll item
+	   var cur = scrollItems.map(function(){
+	   	if ($(this).offset().top < fromTop)
+	   		return this;
+	   });
+	   // Get the id of the current element
+	   cur = cur[cur.length-1];
+	   var id = cur && cur.length ? cur[0].id : "";
+	   // Set/remove active class
+	   $("#resume-navbar").find("a").parent().removeClass("active")
+	   .end().filter("[href='#"+id+"']").parent().addClass("active");
+	});
 });
